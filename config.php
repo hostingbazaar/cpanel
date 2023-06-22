f989b440f3e37e014efdcc8afad3eee086eeafa3
Arpit Vaishnav <hostingbazaar.com@gmail.com>
2023-06-22 18:50:25 +0530

    added config file

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+ $con=mysqli_connect("46.4.122.54","softdbtechserver_wptester","arpit@123","softdbtechserver_wptester");// Check connection
+ if (mysqli_connect_errno()){"Failed to connect : " . mysqli_connect_error();}else{  "connected"; }
+
